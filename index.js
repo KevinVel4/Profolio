ceed523fe1cf65e8fb6ad37e524c8dfd9b0264a5
Kevin Vela <105293805+KevinVel4@users.noreply.github.com>
2022-10-04 12:47:15 -0400

    Updated

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on kevinvelam@gmail.com"
       );
     });
 }
